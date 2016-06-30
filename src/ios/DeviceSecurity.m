80cf52860b2cc934d06516df68430e403e2e3197
awjacobson <awjacobson@aaronjacobson.com>
2016-06-30 13:09:29 -0500

    support iOS 8 and 9

--- src/ios/DeviceSecurity.m
+++ src/ios/DeviceSecurity.m
@@ -6,8 +6,27 @@
 
 - (void)doesDeviceHaveSecuritySetup:(CDVInvokedUrlCommand*)command
 {
-    LAContext *context = [[LAContext alloc] init];
-    BOOL isSecuritySetup = [context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:nil];
+    BOOL isSecuritySetup = false;
+
+    if([LAContext class]) // iOS 9.0+
+    {
+        LAContext *context = [[LAContext alloc] init];
+        isSecuritySetup = [context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:nil];
+    }
+    {
+        NSData* secret = [@"Device has passcode set?" dataUsingEncoding:NSUTF8StringEncoding];
+        NSDictionary *attributes = @{ (__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword,
+            (__bridge id)kSecAttrService: @"LocalDeviceServices",
+            (__bridge id)kSecAttrAccount: @"NoAccount",
+            (__bridge id)kSecValueData: secret,
+            (__bridge id)kSecAttrAccessible: (__bridge id)kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly };
+        OSStatus status = SecItemAdd((__bridge CFDictionaryRef)attributes, NULL);
+        if (status == errSecSuccess) { // item added okay, passcode has been set
+            SecItemDelete((__bridge CFDictionaryRef)attributes);
+            isSecuritySetup = true;
+        }
+    }
+
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:isSecuritySetup];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
